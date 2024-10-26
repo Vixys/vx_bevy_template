dbd686fd44056353b8e2fd7618c2c2af46af65bf
Sébastien Marichal <sebmarichal@gmail.com>
2024-10-26 10:51:13 +0200

    Base template

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use bevy::prelude::*;
+use vx_dev_tools::DevToolsPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill the entire browser window
+                fit_canvas_to_parent: true,
+                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(DevToolsPlugin)
+        .add_systems(Startup, camera_setup)
+        .run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
